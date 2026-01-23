2b415294e9f36e0cc52422edbae6ed7b8aa37890
Jesse Quijano <jesse.quijano@lmco.com>
2026-01-22 16:26:59 -0800

    simple greeting server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mcp-aca
+
+Code created for the Udemy course https://www.udemy.com/share/10eFUx3@_fEgS73cVJTsjFjRqs3qV9SR5ilgIYxVbKDGEydUtg87pW4QmEmVd59X0vaD6y8uiQ==/
---
+++ tool/server.py
@@ -0,0 +1,12 @@
+from mcp.server.fastmcp import FastMCP
+from pydantic import BaseModel
+
+mcp = FastMCP()
+
+class Greeting(BaseModel):
+    message: str
+
+@mcp.tool("Greetings")
+def greetings(name: str) -> Greeting:
+    """A tool function that accepts a parameter called name and resturns a personalized greeting message."""
+    return Greeting(message ="Hello, {name}!")
